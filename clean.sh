9202971f27deb0d991d37293d36d886c18c468ba
Animus PEXUS <pexuone@gmail.com>
2012-10-23 06:29:39 +0300

    remover garbage from root

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find .   '(' -name '*~' -o -name '*#*' -o -name '*.pyc' -o -name '*.pyo' ')' -exec rm -f '{}' ';'
-
-rm -r html
-#bash gen_api_doc.sh
-
-chmod -R u=rwx .
--- editall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# do `emacs --daemon` before starting this script
-find . -maxdepth 2 -name '*.py' -exec bash -c 'emacsclient -c -n {} &' ';'
-find ./templates -maxdepth 1 -name '*.html' -exec bash -c 'emacsclient -c -n {} &' ';'
