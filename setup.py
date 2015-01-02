fc2a182836da6056f4b30e7ee19ff56b05a25548
Animus PEXUS <pexuone@gmail.com>
2015-01-02 05:30:11 +0300

    some fixes. adding support for nftables

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='aipsetup',
-    version='3.0.124',
+    version='3.0.131',
     description='software tools for building and maintaining own gnu+linux distro',
     author='Alexey V Gorshkov',
     author_email='animus@wayround.org',
@@ -26,13 +26,14 @@ setup(
         ],
     package_data={
         'org.wayround.aipsetup': [
+            os.path.join('*.sh'),
             os.path.join('distro', '*.tar.xz'),
             os.path.join('gui', '*.glade'),
             os.path.join('distro', '*.json'),
             os.path.join('distro', '*.sqlite'),
             os.path.join('distro', 'pkg_buildscripts', '*.py'),
             os.path.join('distro', 'pkg_info', '*.json'),
-            os.path.join('distro', 'groups', '*.json'),
+            os.path.join('distro', 'pkg_groups', '*'),
             os.path.join('web', 'src_server', 'templates', '*'),
             os.path.join('web', 'src_server', 'js', '*'),
             os.path.join('web', 'src_server', 'css', '*'),
