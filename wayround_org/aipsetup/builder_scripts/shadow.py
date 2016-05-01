19a25830ce1391d5015188b0e7d63967b689f397
Animus PEXUS <pexuone@gmail.com>
2016-05-01 21:04:12 +0300

    fixes

--- wayround_org/aipsetup/builder_scripts/shadow.py
+++ wayround_org/aipsetup/builder_scripts/shadow.py
@@ -13,6 +13,6 @@ class Builder(wayround_org.aipsetup.builder_scripts.std.Builder):
 
     def builder_action_configure_define_opts(self, called_as, log):
         return super().builder_action_configure_define_opts(called_as, log) + [
-            '--enable-man',
+            # '--enable-man',
             # '--without-selinux'
             ]
