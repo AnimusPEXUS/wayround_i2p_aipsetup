8de2f647f4dfa4ff6391f974b680556b33cffd1b
Animus PEXUS <pexuone@gmail.com>
2016-05-20 12:33:56 +0300

    version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aipsetup',
-    version='3.3.8',
+    version='3.3.9',
     description='software tools for building and maintaining own gnu+linux distro',
     author='Alexey V Gorshkov',
     author_email='animus@wayround.org',
